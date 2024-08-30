61af2be758c2f353dc96297598ce183debb674e5
theblaisecode <theblaisecode@gmail.com>
2024-08-30 07:24:14 +0100

    first commit

--- VanLife/src/Pages/Vans/Vans.jsx
+++ VanLife/src/Pages/Vans/Vans.jsx
@@ -1,9 +1,66 @@
+import { useEffect, useState } from "react";
 import VansWrapper from "./VansWrapper";
 
 function Vans() {
+  const [vans, setVans] = useState([]);
+
+  async function getVans() {
+    try {
+      const res = await fetch("/api/vans");
+      const data = await res.json();
+      setVans(data.vans);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getVans();
+  }, []);
+
+  console.log(vans);
+
   return (
     <VansWrapper>
-      <h3>Vans Page</h3>
+      <div className="allVans">
+        <div className="container">
+          <div className="vansContent">
+            <div className="contentTop">
+              <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+
+              <div className="vanFilter">
+                {vans.map((vanType) => {
+                  const { id, type } = vanType;
+                  return (
+                    <div key={id} className="btn vanTypes">
+                      {type}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="contentBottom">
+              {vans.map((item) => {
+                const { id, imageUrl, name, price, type, description } = item;
+                return (
+                  <div key={id} className="vanCard">
+                    <div className="cardTop">
+                      <img src={imageUrl} alt={name} />
+                    </div>
+
+                    <div className="cardBottom">
+                      <h4 className="title">{name}</h4>
+                      <span className="price">{price}/day</span>
+                      <div className="btn tag">{type}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </VansWrapper>
   );
 }
--- VanLife/src/Pages/Vans/VansWrapper.js
+++ VanLife/src/Pages/Vans/VansWrapper.js
@@ -1,5 +1,32 @@
 import styled from "styled-components";
 
-const VansWrapper = styled.div``;
+const VansWrapper = styled.div`
+  .contentTop {
+    margin: 2rem 0;
+  }
+
+  .vanFilter {
+    margin-top: 2rem;
+    display: flex;
+    gap: 1.2rem;
+  }
+
+  .vanFilter .vanTypes {
+    background: var(--selection);
+    color: var(--textDark);
+  }
+
+  .contentBottom {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 4rem;
+    margin: 5rem 0;
+  }
+
+  .vanCard img {
+    width: 100%;
+    border-radius: 1.5rem;
+  }
+`;
 
 export default VansWrapper;
