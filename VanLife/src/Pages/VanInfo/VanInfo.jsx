542fefe1306c7e36dc2904bb4870df51d7993baf
theblaisecode <theblaisecode@gmail.com>
2024-09-04 19:45:35 +0100

    first commit

--- VanLife/src/Pages/VanInfo/VanInfo.jsx
+++ VanLife/src/Pages/VanInfo/VanInfo.jsx
@@ -1,11 +1,62 @@
+import { Link, useParams } from "react-router-dom";
 import VanInfoWrapper from "./VanInfoWrapper";
+import { useEffect, useState } from "react";
 
 function VanInfo() {
+  const params = useParams();
+  const [vanInfo, setVanInfo] = useState(null);
+
+  async function getVanInfo() {
+    try {
+      const res = await fetch(`/api/vans/${params.id}`);
+      const data = await res.json();
+      console.log(data);
+      setVanInfo(data.vans);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getVanInfo();
+  }, [params.id]);
+
+  console.log(vanInfo);
   return (
     <VanInfoWrapper>
       <div className="vanInfo">
         <div className="container">
-          <h1>Van details goes here</h1>
+          <Link to="/vans" className="back">Back to all vans</Link>
+          {vanInfo ? (
+            <div className="vanInfoContent">
+              <div className="contentTop">
+                <img src={vanInfo.imageUrl} alt={`Image of ${vanInfo.name}`} />
+              </div>
+
+              <div className="contentBottom">
+                <div className={`tag ${vanInfo.type} selected`}>
+                  {vanInfo.type}
+                </div>
+
+                <h2 className="title" style={{ display: "inline-block" }}>
+                  {vanInfo.name}
+                </h2>
+
+                <p className="price">
+                  ${vanInfo.price}
+                  <span className="highlight">/day</span>
+                </p>
+
+                <p className="description">{vanInfo.description}</p>
+
+                <button className="btn">Rent this van</button>
+              </div>
+            </div>
+          ) : (
+            <div className="vanInfo">
+              <div className="loading"></div>
+            </div>
+          )}
         </div>
       </div>
     </VanInfoWrapper>
--- VanLife/src/Pages/VanInfo/VanInfoWrapper.js
+++ VanLife/src/Pages/VanInfo/VanInfoWrapper.js
@@ -1,7 +1,111 @@
 import styled from "styled-components";
 
 const VanInfoWrapper = styled.div`
+  .vanInfo {
+    min-height: 74.8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-`
+  .vanInfoContent {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 5rem;
+    margin: auto;
+    width: 90%;
+  }
+
+  .vanInfoContent .contentTop {
+    flex: 0.8;
+  }
+
+  .vanInfoContent .contentBottom {
+    flex: 0.8;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  .vanInfoContent img {
+    width: 100%;
+    border-radius: 1.5rem;
+  }
+
+  .vanInfoContent .title {
+    font-size: 4rem;
+    font-weight: 700;
+  }
+
+  .vanInfoContent .price {
+    font-size: 2.3rem;
+    font-weight: 700;
+  }
+
+  .vanInfoContent .price .highlight {
+    font-weight: 400;
+    font-size: 2rem;
+  }
+
+  .vanInfoContent .description {
+    font-size: 1.8rem;
+    line-height: 2.5rem;
+  }
+
+  .vanInfoContent .tag {
+    height: 3.4rem;
+    display: inline-flex;
+    align-self: flex-start;
+    padding: 0.6rem 2.6rem;
+    font-weight: 500;
+    background: var(--selection);
+    color: var(--textLight);
+  }
+
+  .vanInfoContent .tag:hover {
+    color: var(--selection);
+  }
+
+  .vanInfoContent .tag:focus {
+    outline: none;
+  }
+
+  .vanInfoContent .tag.selected {
+    color: var(--selection);
+  }
+
+  .vanInfoContent .tag.simple:hover,
+  .vanInfoContent .tag.simple.selected {
+    background: var(--btnOrange);
+  }
+
+  .vanInfoContent .tag.rugged:hover,
+  .vanInfoContent .tag.rugged.selected {
+    background: var(--btnGreen);
+  }
+
+  .vanInfoContent .tag.luxury:hover,
+  .vanInfoContent .tag.luxury.selected {
+    background: var(--textDark);
+  }
+
+  .vanInfoContent .btn {
+    margin-top: 3rem;
+    background: var(--primary);
+    color: var(--textLight);
+    border: none;
+    text-align: center;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    transition: 0.4s;
+    font-size: 1.8rem;
+  }
+
+  .btn:hover {
+    background: var(--accentPrimary);
+    transition: all 0.4s;
+  }
+`;
 
 export default VanInfoWrapper;
