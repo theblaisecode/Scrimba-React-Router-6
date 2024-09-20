61165573a982dc6d8e0406547bf80b085e6e2f67
theblaisecode <theblaisecode@gmail.com>
2024-09-20 07:53:33 +0100

    first commit

--- VanLife/src/Pages/Host/HostVansDetail.jsx
+++ VanLife/src/Pages/Host/HostVansDetail.jsx
@@ -22,11 +22,18 @@ function HostVansDetail() {
         &larr; <span>Back to all vans</span>
       </Link>
 
-      <div>
-        <img src={currentVan.imageUrl} width={150} />
-        <h2>{currentVan.name}</h2>
-        <p>{currentVan.price}</p>
-        <p>{currentVan.type}</p>
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+        {/*Your changes will go here*/}
       </div>
     </>
   );
